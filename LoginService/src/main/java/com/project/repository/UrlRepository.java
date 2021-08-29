d975cd49c76a41c3f6e6f8723796298714f02499
ChiragChiranjib <58375906+ChiragChiranjib@users.noreply.github.com>
2021-08-29 13:38:35 +0530

    Delete LoginService directory

--- LoginService/src/main/java/com/project/repository/UrlRepository.java
+++
@@ -1,7 +0,0 @@
-package com.project.repository;
-
-import com.project.model.UrlResponse;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UrlRepository extends CrudRepository<UrlResponse, Integer> {
-}
--- LoginService/src/main/java/com/project/service/SessionService.java
+++
@@ -1,5 +0,0 @@
-package com.project.service;
-
-public interface SessionService {
-    void saveUrl(String url, String response);
-}
